0c53420527f1f0033f4679d4f1d1b5248c90dac8
Kevin Chen <erf@standarddoubt.com>
2015-03-15 17:54:15 -0400

    Initial Commit

---
+++ RandomNumberGenerator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomNumberGenerator
+//
+//  Created by Kevin Chen on 3/15/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomNumberGenerator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomNumberGenerator
+//
+//  Created by Kevin Chen on 3/15/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
